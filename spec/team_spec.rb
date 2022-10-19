fe24fc70ea787ccedee33fdb3f0456138a0589a5
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-19 14:00:16 -0700

    test: added test for total value method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -43,13 +43,35 @@ RSpec.describe Team do
     team.add_player(player_4)
 
     expect(team.long_term_players).to eq([player_1, player_4])
-    expect(team.short_term_players).to eq([player_2, player_3])
-
   end
-  
 
+  it 'has a method to list short term players' do
+    team = Team.new("Sounders", "Seattle")
+    player_1 = Player.new("Raul Ruidiaz", 10000, 25)
+    player_2 = Player.new("Joao Paulo", 12000, 12)
+    player_3 = Player.new("Fredy Montero", 5000, 3)
+    player_4 = Player.new("Jordan Morris", 9000, 36)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.short_term_players).to eq([player_2, player_3])
+  end
+
+  it 'has a method to give team total value' do
+    team = Team.new("Sounders", "Seattle")
+    player_1 = Player.new("Raul Ruidiaz", 10000, 25)
+    player_2 = Player.new("Joao Paulo", 12000, 12)
+    player_3 = Player.new("Fredy Montero", 5000, 3)
+    player_4 = Player.new("Jordan Morris", 9000, 36)
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
  
-
+    expect(team.total_value).to eq(733000)
+  end
 
 
 end
