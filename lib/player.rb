93f8ed265b8ebaf16a12f4eeb1bbb1b4e0f5396a
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-19 13:22:01 -0700

    feat: completed initial player class and tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,15 @@
 class Player
-  attr_reader :name, :monthly_cost, :contract_length
+  attr_reader :name, :first_name, :last_name, :monthly_cost, :contract_length
 
   def initialize(name, monthly_cost, contract_length)
     @name = name
+    @first_name = name.split(" ")[0]
+    @last_name = name.split(" ")[1]
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+  end
+
+  def total_cost
+    @monthly_cost * @contract_length
   end
 end
